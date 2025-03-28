50d88f1368e88a20003c73636f86a18311ed26d5
LuisFzg39 <luisfzuluaga2004hotmail.com>
2025-03-28 18:00:50 -0500

    Cuarto-commit

--- src/card.js
+++ src/card.js
@@ -1,5 +1,17 @@
 class Card extends HTMLElement {
 
+    constructor(){
+
+        super()
+
+    }
+
+    connectedCallback(){
+
+        const shadow = this.attachShadow({mode:open})
+
+    }
+
     
 
 
